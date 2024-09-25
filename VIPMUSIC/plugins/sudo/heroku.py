d27ffb237171ad64293183b846bfffc4cc664656
Goku malik <168086405+Gokukhan1@users.noreply.github.com>
2024-09-25 10:36:24 +0530

    Update heroku.py

--- VIPMUSIC/plugins/sudo/heroku.py
+++ VIPMUSIC/plugins/sudo/heroku.py
@@ -1,9 +1,9 @@
 #
-# Copyright (C) 2024 by THE-VIP-BOY-OP@Github, < https://github.com/THE-VIP-BOY-OP >.
+# Copyright (C) 2024 by GOKU @Github, < https://github.com/Gokukhan1/Xeno-kakarot- >.
 #
-# This file is part of < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC > project,
+# This file is part of < https://github.com/Gokukhan1/Xeno-kakarot- > project,
 # and is released under the MIT License.
-# Please see < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC/blob/master/LICENSE >
+# Please see < https://github.com/Gokukhan1/Xeno-kakarot-/blob/master/LICENSE >
 #
 # All rights reserved.
 #
