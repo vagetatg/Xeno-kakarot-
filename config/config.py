11c96026f503db12329a9d471f03c823c088d2ba
vagetatg <nibbanmisal3302@gmail.com>
2024-09-27 21:51:25 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -52,7 +52,7 @@ EXTRA_PLUGINS = getenv(
 
 EXTRA_PLUGINS_REPO = getenv(
     "EXTRA_PLUGINS_REPO",
-    "https://github.com/Gokukhan1/Xeno-kakarot-Extra",
+    "https://github.com/vagetatg/Xeno-kakarot-Extra",
 )
 # Fill here the external plugins repo where plugins that you want to load
 
