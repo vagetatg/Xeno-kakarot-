52d72a3ae1e3b7ca29bf89d3b33fce52b41e363d
Hasnain khan <161922960+hasnainkk-07@users.noreply.github.com>
2024-09-27 11:24:33 +0530

    Update mongo.py

--- VIPMUSIC/utils/mongo.py
+++ VIPMUSIC/utils/mongo.py
@@ -8,7 +8,7 @@ from typing import Dict, Union
 
 from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 
-from config import MONGO_DB_URI
+from config.config import MONGO_DB_URI
 
 mongo = MongoCli(MONGO_DB_URI)
 db = mongo.VIPMUSIC
